885ec0751d90b26ad944e279669be1c45b1aab60
Rohan Yadav <yrohan740@gmail.com>
2022-10-31 21:32:51 +0530

    #5: Optimization

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,45 +1,45 @@
 import About from './About'
 import {useState} from 'react';
 import { createSearchParams,useNavigate} from 'react-router-dom';
-
 const Home = () => {
-
     const [url,setUrl] = useState(null);
-    const [data,setData] = useState(null);
     let navigate = useNavigate();
 
     const handleChange = (e) =>{
         e.preventDefault();
         if(validateUrl(url)){
-            sendRequest();
+            navigate({
+                pathname:'/summarize',
+                search:createSearchParams({
+                    url:url,
+                }).toString()
+        }); 
         }else{
             alert("Invalid Url");
         }
     }
 
-    async function sendRequest(){
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            crossDomain:true,
-            body: JSON.stringify({url})
-        };
-        const response = await fetch('http://localhost:7000/summarize', requestOptions);
-        setData(await response.json());
-        setTimeout('',100);
-        console.log(data);
-        if(data){
-            navigate({
-                pathname:'/summarize',
-                search:createSearchParams({
-                    url:data.url,
-                    summary:data.summary,
-                    top_image:data.top_image,
-                    title:data.title,
-                }).toString()
-        });
-        }
-    }
+    // async function sendRequest(){
+        // const requestOptions = {
+        //     url:'http://localhost:7000/summarize',
+        //     method: 'POST',
+        //     headers: { 
+        //         'Content-Type': 'application/json' 
+        //     },
+        //     body: JSON.stringify({url})
+        // };
+        // const response = await fetch('http://localhost:7000/summarize', requestOptions);
+        // setData(await response.json());
+        // setTimeout('',100);
+        // console.log(data);
+
+    // }
+
+    // function red() { 
+    //     if(data){
+            
+    //     }    
+    // }
 
     function validateUrl(value) {
         return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
@@ -58,6 +58,7 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+           
             <div id="input" className="mx-16">
                 <form className="w-auto font-satoshi" onSubmit={handleChange}>
                     <div class="mb-6 ">
@@ -66,7 +67,9 @@ const Home = () => {
                         </div>
                         <input onChange={event =>setUrl(event.target.value)} className="bg-white text-gray-900 text-xl rounded-xl block p-2.5 px-7 mx-auto w-2/3 h-20 outline-none font-medium" placeholder="https://....." type="text"></input>
                     </div>
-                    <input type="submit" value="Summarize" className="w-56 h-16 rounded-xl text-2xl text-white font-semibold cursor-pointer tracking-wide" style={{"backgroundColor":"#30A0DF"}}></input>
+                    <button type="submit" className="w-56 h-16 rounded-xl text-2xl text-white font-semibold cursor-pointer tracking-wide" style={{"backgroundColor":"#30A0DF"}}>
+                        <span className="">Summarize</span>
+                    </button>
                 </form>
             </div>
         </div>
